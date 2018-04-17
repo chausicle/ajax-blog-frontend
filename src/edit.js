662ae5f0b21ca33ac7a733957cf4270b56c910aa
Justin Chau <chausicle@gmail.com>
2018-04-16 18:39:59 -0700

    Make change to code

--- src/edit.js
+++ src/edit.js
@@ -26,45 +26,46 @@ document.querySelector("#edit-post").addEventListener("click", (event) => {
   });
 })
 
+// document.addEventListener("DOMContentLoaded", () => {
+// });
+document.querySelector("#update-button").addEventListener("click", (event) => {
+  event.preventDefault();
+  const updateView = document.querySelector("#update-form");
+  const updateTitle = updateView.querySelector("#update-title");
+  const updateContent = updateView.querySelector("#update-content");
+  const activeTab = document.querySelector(".active");
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#update-button").addEventListener("click", (event) => {
-    event.preventDefault();
-    const updateView = document.querySelector("#update-form");
-    const updateTitle = updateView.querySelector("#update-title");
-    const updateContent = updateView.querySelector("#update-content");
+  // find id from href
+  // let url = window.location.href.split("/posts/");
+  let url = event.target.href.split("/posts/");
+  let id = url[1].split("/edit")[0];
 
-    // find id from href
-    let url = window.location.href.split("/posts/");
-    let id = url[1].split("/edit")[0];
+  // update existing post with new title and content
+  axios.put(`${baseURL}/posts/${id}`, {
+    title: updateTitle.value,
+    content: updateContent.value
+  })
+  .then(result => {
+    let alert = document.querySelector(".alert");
 
-    // update existing post with new title and content
-    axios.put(`${baseURL}/posts/${id}`, {
-      title: updateTitle.value,
-      content: updateContent.value
+    // make the form and alert display none when a new post is successfully created
+    alert.style.display = "none";
+    updateView.style.display = "none";
+
+    // reload page to get new content to show
+    location.assign("index.html");
+  })
+  .catch(error => {
+    // error response
+    let alert = document.querySelector(".alert");
+    const response = error.response.data.error;
+
+    // apply error messages to alert text content
+    alert.textContent = response.message;
+    response.errors.forEach((error, index, array) => {
+      if (index === array.length - 1) alert.textContent += `${error}`;
+      else alert.textContent += `${error}, `;
     })
-    .then(result => {
-      let alert = document.querySelector(".alert");
-
-      // make the form and alert display none when a new post is successfully created
-      alert.style.display = "none";
-      updateView.style.display = "none";
-
-      // reload page to get new content to show
-      location.assign("index.html");
-    })
-    .catch(error => {
-      // error response
-      let alert = document.querySelector(".alert");
-      const response = error.response.data.error;
-
-      // apply error messages to alert text content
-      alert.textContent = response.message;
-      response.errors.forEach((error, index, array) => {
-        if (index === array.length - 1) alert.textContent += `${error}`;
-        else alert.textContent += `${error}, `;
-      })
-      alert.style.display = "block";
-    });
+    alert.style.display = "block";
   });
 });
